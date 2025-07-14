600eeaa391e739e8d75b489b12589ac85b8dd3a9
Imtious-Coder <iarafat386@gmail.com>
2025-07-14 13:09:30 +0600

    :construction: Problem 3 solved.

--- Problems.js
+++ Problems.js
@@ -1,4 +1,4 @@
-// Problem 1 Array Filtering and Mapping
+// Problem 1 Array Filtering and Mapping - Solution
 const teamMembers = [
   { name: "Alice", age: 25, gender: "female" },
   { name: "Robert", age: 30, gender: "male" },
@@ -14,7 +14,7 @@ const extractMaleNames = (members) => {
 // console.log(extractMaleNames(teamMembers));
 
 
-// Problem 2 Object Manipulation
+// Problem 2 Object Manipulation - Solution
 const libraryShelf = [
   { title: "Design Patterns", author: "Erich Gamma", year: 1994 },
   { title: "Refactoring", author: "Martin Fowler", year: 1999 },
@@ -22,5 +22,15 @@ const libraryShelf = [
 ];
 
 const listBookTitles = (booksArray) => booksArray.map(book => book.title);
-
 // console.log(listBookTitles(libraryShelf));
+
+
+// Problem 3 Function Composition - Solution
+const squareValue = (n) => n * n;
+const doubleValue = (n) => n * 2;
+const incrementByFive = (n) => n + 5;
+
+const processNumber = (inputNumber) => {
+  return incrementByFive(doubleValue(squareValue(inputNumber)));
+};
+// console.log(processNumber(4));
